6e7048880546371bdf35066215bdf1ac9a0c0f3f
Zachary <zo49@drexel.edu>
2024-12-06 20:36:57 -0500

    Initial Changes

--- LaunchCore.py
+++ LaunchCore.py
@@ -5,50 +5,48 @@ from NeuralNetwork import NeuralNetwork
 import NetworkCore
 import GeneticAlgoCore
 
-xInputs = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 2, 1))
-yExpected = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
+from keras.api.datasets import mnist
+from keras.src.utils.numerical_utils import to_categorical
 
-# Backpropagation and typical training behavior
+def preprocess_data(x, y, limit):
+    # reshape and normalize input data
+    x = x.reshape(x.shape[0], 28 * 28, 1)
+    x = x.astype("float32") / 255
+    # encode output which is a number in range [0,9] into a vector of size 10
+    # e.g. number 3 will become [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+    y = to_categorical(y)
+    y = y.reshape(y.shape[0], 10, 1)
+    return x[:limit], y[:limit]
 
-# network = [
-#     DenseLayer.DenseLayer(2, 3),
-#     ActivationLayers.aTanh(),
-#     DenseLayer.DenseLayer(3, 1),
-#     ActivationLayers.aTanh()
-# ]
-    
-# NetworkCore.train(network, NetworkCore.MeanSquaredError, NetworkCore.MeanSquaredErrorPrime, xInputs, yExpected, epochs=1000, learning_rate=0.1)
 
-# # test
-# for x, y in zip(xInputs, yExpected):
-#     output = NetworkCore.predict(network, x)
-#     print('pred:', abs(round(np.max(output), 3)), '\ttrue:', np.max(y))
-    
-#Genetic Algorithm
+# load MNIST from server
+(xTrain, yTrain), (xTest, yTest) = mnist.load_data()
+xTrain, yTrain = preprocess_data(xTrain, yTrain, 1000)
+xTest, yTest = preprocess_data(xTest, yTest, 20)
 
 population = []
 
 for index in range(200):
     network = NeuralNetwork(
         [
-            DenseLayer.DenseLayer(2, 3),
+            DenseLayer.DenseLayer(28 * 28, 40),
             ActivationLayers.aTanh(),
-            DenseLayer.DenseLayer(3, 1),
+            DenseLayer.DenseLayer(40, 10),
             ActivationLayers.aTanh()
         ]
     )
     population.append(network)
     
-population = GeneticAlgoCore.trainGenetically(population, GeneticAlgoCore.ErrorBasedFitness, xInputs, yExpected, generationLimit=1000)
+population = GeneticAlgoCore.trainGenetically(population, GeneticAlgoCore.ErrorBasedFitness, xTrain, yTrain, generationLimit=1000)
 
 for network in population:
     print(network.fitnessScore)
-    for x, y in zip(xInputs, yExpected):
+    for x, y in zip(xTest, yTest):
         output = NetworkCore.predict(network.layers, x)
         # print('pred:', np.argmax(output), '\ttrue:', np.argmax(y))
-        print('Input:', x[0], x[0], 'pred:', abs(round(np.max(output), 3)), '\ttrue:', y[0][0])
+        print('pred:', np.argmax(output), '\ttrue:', np.argmax(y))
         print('Error:', round(NetworkCore.MeanSquaredError(y, output), 3)*100)
-    print("Done with network")
+    print("Done with network")  
     input("Waiting...")
     
 print("Done")
